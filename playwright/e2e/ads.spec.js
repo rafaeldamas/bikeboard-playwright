61852891ebd0a43cb5037ead245b326a2af51ff2
Rafael Damas <rafael.damas99@gmail.com>
2025-11-29 05:16:12 -0300

    screenshots e videos para todos os testes

--- playwright/e2e/ads.spec.js
+++ playwright/e2e/ads.spec.js
@@ -70,7 +70,7 @@ test.describe("Cadastro de Anúncio", () => {
     await actions.submitAdForm()
 
     await expect(
-      page.locator("text=WhatsApp deve ter menos de 10 ou 11 dígitos")
+      page.locator("text=WhatsApp deve ter 10 ou 11 dígitos")
     ).toBeVisible()
   })
 
